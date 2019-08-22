45efaf3672291732a41aa2f8f476a4dc34e1055f
onux <oanystefy1>
2019-08-22 18:53:37 +0300

    Initial commit

---
+++ src/test/java/org/fasttrack/utils/Constants.java
@@ -0,0 +1,9 @@
+package org.fasttrack.utils;
+
+public class Constants {
+
+    public static final String USER_EMAIL = "oanav94@yahoo.com";
+    public static final String USER_PASS = "123456";
+    public static final String USER_NAME = "ana ana";
+
+}
